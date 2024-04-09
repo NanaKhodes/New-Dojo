8387c8dde6e00c02500e58c5fa684a45135612bc
Nancy Ekwere <99981194+NanaKhodes@users.noreply.github.com>
2024-04-09 17:28:01 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './Navbar';
+
+function App() { 
+  return (
+    <div className="App">
+      <div className="content">
+       <h1>{ title }</h1>
+       <p>Liked { likes } times </p>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
